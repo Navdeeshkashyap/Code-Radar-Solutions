1ac7940fa399d87273709ee4f2a4be11505db5c8
Navdeesh <kashyapnavdeesh@gmail.com>
2025-02-16 13:09:33 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -3,21 +3,22 @@ int main(){
 int a ,b;
 char op;
 scanf("%d %d %c",&a,&b,&op);
+int result;
 switch(op){
     case'+':
-     int result=a+b;
+    result=a+b;
     printf("%d",result);
     break;
 case'-':
-     int result=a-b;
+     result=a-b;
     printf("%d",result);
 break;
 case'*':
-     int result=a*b;
+     result=a*b;
     printf("%d",result);
 break;
 case'/':
-     int result=a/b;
+     result=a/b;
     printf("%d",result);
 break;
 }
